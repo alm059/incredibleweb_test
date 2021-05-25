fa6f84a99e7dd65bf9f23f6541d1101d8464c112
aleelzomtn <1936162+alm059@users.noreply.github.com>
2021-05-25 18:31:53 +0100

    Baseline react

---
+++ frontend/src/page_list.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Page extends React.Component {
+
+}
+
+
+ReactDOM.render(<Page number='1' />, document.getElementById('toggable_panel'));
