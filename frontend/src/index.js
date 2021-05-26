2dcc47557c7b2f65f8ee0b7858672e3aa16b169c
aleelzomtn <1936162+alm059@users.noreply.github.com>
2021-05-26 03:01:54 +0100

    Ordered

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Movies } from './presentation/movies.js'
 import { SearchBar } from './presentation/filters.js'
+import styles from './index.module.css';
 
+class Page extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			selectedMovie: -1,
+		};
+	}
+	openMovieData(id){
+		this.setState({selectedMovie: id});
+	}
+	render(){
+		return (
+			<div id="container">
+				<div id={styles.search_panel}>
+					<SearchBar />
+				</div>
+			    <nav id={styles.toggable_panel}></nav>
+			    <div id={styles.main_panel}>
+					<Movies onClick={id => this.openMovieData(id)} selectedMovie={this.state.selectedMovie} />
+				</div>
+			</div>
+		);
+	}
+}
 
-ReactDOM.render(<SearchBar />, document.getElementById('search_panel'));
-ReactDOM.render(<Movies />, document.getElementById('main_panel'));
+ReactDOM.render(<Page />, document.getElementById('root'));
--- frontend/src/presentation/movies.js
+++ frontend/src/presentation/movies.js
@@ -12,27 +12,20 @@ class Movie extends React.Component {
 			year: 0,
 			category: "No category",
 			rating: "No rating",
-			dataOpen: false,
+			id: -1,
 		};
 	}
 	static getDerivedStateFromProps(props, state){
-		return {title: props.title, year: props.year, image: props.id+".png", category: props.category, rating: props.rating};
-	}
-	displayData = () => {
-		if(this.state.dataOpen){
-			this.setState({dataOpen: false});
-		}else{
-			this.setState({dataOpen: true});
-		}
+		return {id: props.id, title: props.title, year: props.year, image: props.id+".png", category: props.category, rating: props.rating};
 	}
 	openMovie = () => {
 
 	}
 	render(){
-		const movieBoxStyle = (this.state.dataOpen ? {boxShadow: "0px 4px 8px 0px inset #282828",}:{boxShadow: "3px 3px 5px 6px #282828"});
-		const movieBoxDataStyle = (this.state.dataOpen ? {display: "block"}:{display: "none"});
+		const movieBoxStyle = (this.props.dataOpen ? {boxShadow: "0px 4px 8px 0px inset #282828",}:{boxShadow: "3px 3px 5px 6px #282828"});
+		const movieBoxDataStyle = (this.props.dataOpen ? {display: "block"}:{display: "none"});
 
-		return (<div class={styles.movie_box} style={movieBoxStyle} onClick={this.displayData}>
+		return (<div class={styles.movie_box} style={movieBoxStyle} onClick={this.props.onClick}>
 					<img src={this.state.image} />
 					<div class={styles.movie_box_title}>{this.state.title}</div>
 					<div class={styles.movie_box_data} style={movieBoxDataStyle}>
@@ -45,16 +38,29 @@ class Movie extends React.Component {
 	}
 }
 
-export function Movies(){
+export function Movies(props){
 	// Load movies from data layer
 	var moviesJSON = GetMovieData();
-	// const numbers = [1, 2, 3, 4, 5];
 	const movies = [];
-	// foreach ({element : moviesJSON}){
-	// 	let test = <Movie title={element.title} />;
-	// }
+	// Sort from highest to lowest in ratings
+	let sorted = false;
+	while(!sorted) {
+		sorted = true;
+		for(var i=1;i<Object.keys(moviesJSON).length;i++) {
+			if(moviesJSON[i].rating > moviesJSON[i-1].rating) {
+				let temp = moviesJSON[i];
+				moviesJSON[i] = moviesJSON[i-1];
+				moviesJSON[i-1] = temp;
+				sorted = false;
+			}
+		}
+	}
 	for (const key in moviesJSON){
-		movies.push(<Movie title={moviesJSON[key].title} year={moviesJSON[key].year} image={key} category={moviesJSON[key].category} rating={moviesJSON[key].rating} />);
+		let dataOpen = false;
+		if(moviesJSON[key].id == props.selectedMovie){
+			dataOpen = true;
+		}
+		movies.push(<Movie onClick={() => props.onClick(moviesJSON[key].id)} dataOpen={dataOpen} title={moviesJSON[key].title} year={moviesJSON[key].year} id={moviesJSON[key].id} category={moviesJSON[key].category} rating={moviesJSON[key].rating} />);
 	}
 	return (
 		<div>
