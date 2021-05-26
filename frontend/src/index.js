1dbb8e33c3c41bab32b2738d08194b49eff0f3f2
aleelzomtn <1936162+alm059@users.noreply.github.com>
2021-05-26 12:45:25 +0100

    First nav

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ class Page extends React.Component {
 				rating: "",
 				year: "",
 			},
+			navbarOpen: false,
 		};
 	}
 	openMovieData(id){
@@ -38,16 +39,33 @@ class Page extends React.Component {
 		}
 		this.setState({searchFilter: newFilter});
 	}
+	toggleNav(){
+		if(this.state.navbarOpen){
+			this.setState({navbarOpen: false});
+		}else{
+			this.setState({navbarOpen: true});
+		}
+	}
 	render(){
+		var panelLocation = "0";
+		var toggleButtonLocation = "90%";
+		var button = "<";
+		if(!this.state.navbarOpen){
+			panelLocation = "-100%";
+			toggleButtonLocation = "105%";
+			button = ">";
+		}
 		return (
 			<div id="container">
 			<Router>
 				<div id={styles.search_panel}>
 					<SearchBar onKeyDown={filters => this.updateSearch(filters)} />
 				</div>
-			    <nav id={styles.toggable_panel}>
+			    <nav id={styles.toggable_panel} style={{left: panelLocation}}>
+					<div id={styles.toggle_panel_button} onClick={() => this.toggleNav()} style={{left: toggleButtonLocation}}>{button}</div>
 					<ul>
-						<li><Link to ="/about">About us</Link></li>
+						<li><Link onClick={() => this.toggleNav()} to="/" class="link">Main menu</Link></li>
+						<li><Link onClick={() => this.toggleNav()} to="/about" class="link">About us</Link></li>
 					</ul>
 				</nav>
 			    <div id={styles.main_panel}>
